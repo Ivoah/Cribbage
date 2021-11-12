96b05d95a16f23ea9dbb2c7868018a6afbff8aae
Noah Rosamilia <ivoahivoah@gmail.com>
2021-11-12 15:26:18 -0500

    Fix run scoring

--- src/main/scala/Hand.scala
+++ src/main/scala/Hand.scala
@@ -11,10 +11,13 @@ case class Hand(cards: Seq[Card]) {
     (1 to cards.length).flatMap(n => cards.combinations(n)).count(_.map(_.scoreValue).sum == 15)*2
   private def pairs(cards: Seq[Card]): Int =
     cards.combinations(2).count{case Seq(c1, c2) => c1.value == c2.value}*2
-  private def run(cards: Seq[Card]): Int =
-    (3 to cards.length).filter(n => cards.map(_.value).sorted.sliding(n).exists(_.sliding(2).forall{case Seq(c1, c2) => c1 + 1 == c2})).maxOption.getOrElse(0)
+  private def run(cards: Seq[Card]): Int = {
+    val runs = (3 to cards.length).flatMap(n => cards.combinations(n).filter(_.map(_.value).sorted.sliding(n).exists(_.sliding(2).forall{case Seq(c1, c2) => c1 + 1 == c2})))
+    runs.filterNot(s => runs.exists(l => s.length < l.length && s.forall(l.contains))).map(_.length).sum
+  }
+
   private def flush(cards: Seq[Card], starter: Option[Card] = None): Int =
-    (if (cards.forall(_.suite == cards.head.suite)) cards.length + (if (starter.exists(_.suite == cards.head.suite)) 1 else 0) else 0)
+    if (cards.forall(_.suite == cards.head.suite)) cards.length + (if (starter.exists(_.suite == cards.head.suite)) 1 else 0) else 0
   private def jack(cards: Seq[Card], starter: Card): Int =
     if (cards.exists(c => c.value == 11 && c.suite == starter.suite)) 1 else 0
 }
